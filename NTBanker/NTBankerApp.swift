ebd365ba916b2babc001590e32e078a1b6bb9369
Javier Munoz <20247642+0xJavier@users.noreply.github.com>
2023-08-01 10:39:39 -0400

    add initial project

---
+++ NTBanker/NTBankerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTBankerApp.swift
+//  NTBanker
+//
+//  Created by Javier Munoz on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct NTBankerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
