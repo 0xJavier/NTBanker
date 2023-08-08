3c440a586ec26a196567204ed9ef6a7c436ae9e4
Javier Munoz <20247642+0xJavier@users.noreply.github.com>
2023-08-08 15:10:48 -0400

    add home tab view

---
+++ NTBanker/Features/HomeTab/HomeTabView.swift
@@ -0,0 +1,51 @@
+//
+//  HomeTabView.swift
+//  NTBanker
+//
+//  Created by Javier Munoz on 8/8/23.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        TabView {
+            HomeView(
+                store: Store(initialState: HomeFeature.State()) {
+                    HomeFeature()
+                }
+            )
+            .tabItem {
+                Label("Home", systemImage: SFSymbols.house.imageString)
+            }
+            
+            LotteryView(
+                store: Store(initialState: LotteryFeature.State()) {
+                    LotteryFeature()
+                }
+            )
+            .tabItem {
+                Label("Lottery", systemImage: SFSymbols.dollarSignSquare.imageString)
+            }
+            
+            RankingView(
+                store: Store(initialState: RankingFeature.State()) {
+                    RankingFeature()
+                }
+            )
+            .tabItem {
+                Label("Ranking", systemImage: SFSymbols.personGroup.imageString)
+            }
+            
+            Text("Settings")
+                .tabItem {
+                    Label("Settings", systemImage: SFSymbols.gear.imageString)
+                }
+        }
+    }
+}
+
+#Preview {
+    HomeTabView()
+}
--- NTBanker/Types/SFSymbols.swift
+++ NTBanker/Types/SFSymbols.swift
@@ -9,11 +9,23 @@ import Foundation
 
 enum SFSymbols {
     case dollarSignCircle
+    case dollarSignSquare
+    case house
+    case personGroup
+    case gear
     
     var imageString: String {
         switch self {
         case .dollarSignCircle:
             return "dollarsign.circle.fill"
+        case .dollarSignSquare:
+            return "dollarsign.square"
+        case .house:
+            return "house"
+        case .personGroup:
+            return "person.2"
+        case .gear:
+            return "gear"
         }
     }
 }
