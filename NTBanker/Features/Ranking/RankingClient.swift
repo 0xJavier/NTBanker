2785415a5f41aaae2812972b29512d4f850f1536
Javier Munoz <20247642+0xJavier@users.noreply.github.com>
2023-08-22 11:35:14 -0400

    fix ranking client comment

--- NTBanker/Features/Ranking/RankingClient.swift
+++ NTBanker/Features/Ranking/RankingClient.swift
@@ -7,14 +7,14 @@
 
 import ComposableArchitecture
 
-/// Main interface for the `RankingFeature`.
+/// Main interface for the `RankingClient`
 struct RankingClient: DependencyKey {
-    /// Calls to Firebase and returns a ordered list of active players in descending order based on a player's balance.
+    /// Calls to Firebase and returns a ordered list of active players in descending order based on a player's balance
     var fetchUsers: @Sendable () async throws -> [User]
 }
 
 extension DependencyValues {
-    /// Registers `RankingClient` with TCA's dependency injection system.
+    /// Registers `RankingClient` with TCA's dependency injection system
     var rankingClient: RankingClient {
         get { self[RankingClient.self] }
         set { self[RankingClient.self] = newValue }
