d496c1500786f251fe86df08e600c4b7d1986578
Javier Munoz <0xjmunoz@gmail.com>
2024-01-02 19:37:11 -0500

    update user model

--- NTBanker/Models/User.swift
+++ NTBanker/Models/User.swift
@@ -9,17 +9,15 @@ import SwiftUI
 
 /// Object representing a user / player
 struct User: Identifiable ,Equatable, Hashable, Codable {
-    /// User's unique UID created from Firebase
     var id: String
-    /// User's name
     var name: String
-    /// User's current game balance
+    var email: String
     var balance: Int
-    /// User's selected card color.
     var color: CardColor
     
     enum CodingKeys: String, CodingKey {
-        case id, name, balance, color
+        case id = "userID"
+        case name, email, balance, color
     }
 }
 
@@ -28,14 +26,15 @@ extension User {
     static let placeholder = User(
         id: "1",
         name: "Player",
+        email: "placeholder@ntbanker.com",
         balance: 0,
         color: .blue
     )
     
     /// Mock list of users used for SwiftUI Previews
     static let mockUserList = [
-        User(id: "1", name: "Player1", balance: 2750, color: .blue),
-        User(id: "2", name: "Player2", balance: 1500, color: .red),
-        User(id: "3", name: "Player3", balance: 750, color: .green)
+        User(id: "1", name: "Player1", email: "a@b.com", balance: 2750, color: .blue),
+        User(id: "2", name: "Player2", email: "b@c.com", balance: 1500, color: .red),
+        User(id: "3", name: "Player3", email: "c@d.com", balance: 750, color: .green)
     ]
 }
